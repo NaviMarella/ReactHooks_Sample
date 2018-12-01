739be9cba99be82789a3b54436886bba385729d8
Navi Marella <navi.marella@gmail.com>
2018-12-01 13:43:08 -0700

    common reusable hooks

--- src/shared/hooks/common-hooks.js
+++ src/shared/hooks/common-hooks.js
@@ -1,6 +1,7 @@
 import {
     useState,
-    useEffect
+    useEffect,
+    useRef
 } from 'react';
 
 export const useDebounce = function (value, delay) {
@@ -19,4 +20,73 @@ export const useDebounce = function (value, delay) {
     );
 
     return debouncedValue;
+}
+
+
+export const usePrevious = function(value) {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  // Return previous value
+  return ref.current;
+}
+
+
+export const useWindowSize = function() {
+    const isClient = typeof window === 'object';
+
+    function getSize() {
+        return {
+            width: isClient ? window.innerWidth : undefined,
+            height: isClient ? window.innerHeight : undefined
+        };
+    }
+
+    const [windowSize, setWindowSize] = useState(getSize);
+
+    useEffect(() => {
+        if (!isClient) {
+            return false;
+        }
+
+        function handleResize() {
+            setWindowSize(getSize());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowSize;
+}
+
+
+export const useHover = function() {
+    const [value, setValue] = useState(false);
+
+    const ref = useRef(null);
+
+    const handleMouseOver = () => setValue(true);
+    const handleMouseOut = () => setValue(false);
+
+    useEffect(
+        () => {
+            const node = ref.current;
+            if (node) {
+                node.addEventListener('mouseover', handleMouseOver);
+                node.addEventListener('mouseout', handleMouseOut);
+
+                return () => {
+                    node.removeEventListener('mouseover', handleMouseOver);
+                    node.removeEventListener('mouseout', handleMouseOut);
+                };
+            }
+        },
+        [ref.current] // Recall only if ref changes
+    );
+
+    return [ref, value];
 }
